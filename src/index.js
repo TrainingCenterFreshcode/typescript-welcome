2aaf6363fc5192dc6ba50065245e60613a27962f
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-05-29 19:34:58 +0300

    Buying alcohol practice task resolve

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
-// const firstName: string = 'Roman';
-// const lastName: string = 'Ivanov';
-// const pi: number = 3.14;
-// console.log(`${firstName} ${lastName}`);
-// // + Вивести 10 консоль.логів у консоль // + Вивести тільки парні
-// for(let i = 0; i < 10; i++) {
-//     if(i % 2 === 0) {
-//         console.log(`Число з циклу for - ${i}`);
-//     }
-// }
-// console.dir(document);
-// Задача: створити невеличкий додаток, який може обчислити факторіал числа
-// Факторіал числа - добуток всіх цілих чисел від 1 до цього числа включно
-// 5! = 1 * 2 * 3 * 4 * 5 = 120
-const form = document.querySelector('#factorial-form');
-const stepsList = document.querySelector('#factorial-steps');
+/*
+
+Задача:
+
+Створіть форму з двома інпутами в яких запитуйте ім'я користувача і його вік.
+У формі має бути кнопка "Купити алкоголь".
+
+Відповідно, якщо у користувача вік легальний - ви виводите повідомлення у верстку з повідомленням про те,
+що алкоголь купувати дозволено.
+Якщо користувач не досягнув відповідного віку - виводите у верстку повідомлення про те,
+що алкоголь купувати не дозволено
+
+*/
+const form = document.querySelector('#alcohol-form');
+const messageDiv = document.querySelector('#message');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    // Очищаємо список перед виведенням нових результатів
-    stepsList.textContent = '';
-    // Отримати введене число
-    const numberInput = document.querySelector('#number');
-    const number = Number(numberInput.value);
-    // Обчисляємо факторіал та додавання кожної ітерації до списку
-    let factorial = 1;
-    for (let i = 1; i <= number; i++) {
-        factorial *= i;
-        const listItem = document.createElement('li');
-        listItem.textContent = `${factorial}`;
-        stepsList.appendChild(listItem);
+    const nameInput = document.querySelector('#name');
+    const ageInput = document.querySelector('#age');
+    const name = nameInput.value;
+    const age = Number(ageInput.value);
+    if (age >= 18) {
+        messageDiv.textContent = `${name}, Вам дозволено купувати алкоголь.`;
+    }
+    else {
+        messageDiv.textContent = `${name}, Вам заборонено купувати алкоголь.`;
     }
 });
