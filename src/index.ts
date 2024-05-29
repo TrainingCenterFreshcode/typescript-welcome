417aed7b95c6cd19472c169d52ccb4cf691d22b6
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-05-29 19:06:38 +0300

    Factorial calculator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log('Full automation - done');
+// const firstName: string = 'Roman';
+// const lastName: string = 'Ivanov';
+
+// const pi: number = 3.14;
+
+// console.log(`${firstName} ${lastName}`);
+
+// // + Вивести 10 консоль.логів у консоль // + Вивести тільки парні
+
+// for(let i = 0; i < 10; i++) {
+//     if(i % 2 === 0) {
+//         console.log(`Число з циклу for - ${i}`);
+//     }
+// }
+
+// console.dir(document);
+
+
+// Задача: створити невеличкий додаток, який може обчислити факторіал числа
+// Факторіал числа - добуток всіх цілих чисел від 1 до цього числа включно
+// 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+const form = document.querySelector('#factorial-form') as HTMLFormElement;
+const stepsList = document.querySelector('#factorial-steps') as HTMLOListElement;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // Очищаємо список перед виведенням нових результатів
+    stepsList.textContent = '';
+
+    // Отримати введене число
+    const numberInput = document.querySelector('#number') as HTMLInputElement;
+    const number: number = Number(numberInput.value);
+
+    // Обчисляємо факторіал та додавання кожної ітерації до списку
+    let factorial: number = 1;
+    for(let i = 1; i <= number; i++) {
+        factorial *= i;
+
+        const listItem: HTMLLIElement = document.createElement('li');
+        listItem.textContent = `${factorial}`;
+        stepsList.appendChild(listItem);
+    }
+});
