03acd6ab93f849e303be8a2760fce49e6663e910
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-06-05 16:33:52 +0300

    Enum

--- src/index.js
+++ src/index.js
@@ -1,39 +1,9 @@
-/*
-
-Задача: Список студентів і найкращий студент
-
-Створіть программу для університету, яка відображатиме інформацію про студентів
-та знаходитиме студента з найвищим середнім балом.
-
-Декомпозиція.
-
-+ 1. Створіть тип даних Student, який містить ім'я студента (name), курс (course), середній бал (averageGrade)
-+ 2. Створіть масив students, що містить об'єкти типу Student з даними про студентів.
-+ 3. Обчисліть середній бал серед усіх студентів та виведіть його у консоль.
-+ 4. Знайдіть студента з найвищим середнім балом та виведіть його дані у консоль.
-
-*/
-const students = [
-    { name: 'Марія', course: 2, averageGrade: 4.5 },
-    { name: 'Іван', course: 3, averageGrade: 4.1 },
-    { name: 'Олександр', course: 1, averageGrade: 4.8 }
-];
-let totalAverage = 0; // загальний середній бал
-let highestAverageStudent = null; // студент з найвищим середнім балом
-let highestAverage = 0; // найвищий середній бал
-let sum = 0;
-for (let student of students) {
-    sum += student.averageGrade;
-    if (student.averageGrade > highestAverage) {
-        highestAverage = student.averageGrade;
-        highestAverageStudent = student;
-    }
-}
-totalAverage = sum / students.length;
-console.log(`Середній бал серед усіх студентів: ${totalAverage}`);
-if (highestAverageStudent !== null) {
-    console.log(`Студент з найвищим середнім балом: `, highestAverageStudent);
-}
-else {
-    console.log('Студент з найвищим середнім балом не знайдений!');
-}
+var AccessLevel;
+(function (AccessLevel) {
+    AccessLevel["Guest"] = "\u0413\u0456\u0441\u0442\u044C";
+    AccessLevel["User"] = "\u041A\u043E\u0440\u0438\u0441\u0442\u0443\u0432\u0430\u0447";
+    AccessLevel["Moderator"] = "\u041C\u043E\u0434\u0435\u0440\u0430\u0442\u043E\u0440";
+    AccessLevel["Admin"] = "\u0410\u0434\u043C\u0456\u043D\u0456\u0441\u0442\u0440\u0430\u0442\u043E\u0440";
+})(AccessLevel || (AccessLevel = {}));
+const currentLevel = AccessLevel.Moderator;
+console.log(`Ваш рівень доступу: ${currentLevel}`);
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,10 @@
-/*
-
-Задача: Список студентів і найкращий студент
-
-Створіть программу для університету, яка відображатиме інформацію про студентів
-та знаходитиме студента з найвищим середнім балом.
-
-Декомпозиція.
-
-+ 1. Створіть тип даних Student, який містить ім'я студента (name), курс (course), середній бал (averageGrade)
-+ 2. Створіть масив students, що містить об'єкти типу Student з даними про студентів.
-+ 3. Обчисліть середній бал серед усіх студентів та виведіть його у консоль.
-+ 4. Знайдіть студента з найвищим середнім балом та виведіть його дані у консоль.
-
-*/
-
-type Student = {
-    name: string;
-    course: number;
-    averageGrade: number
-};
-
-const students: Student[] = [
-    { name: 'Марія', course: 2, averageGrade: 4.5 },
-    { name: 'Іван', course: 3, averageGrade: 4.1 },
-    { name: 'Олександр', course: 1, averageGrade: 4.8 }
-];
-
-let totalAverage: number = 0; // загальний середній бал
-let highestAverageStudent: Student | null = null; // студент з найвищим середнім балом
-let highestAverage: number = 0; // найвищий середній бал
-
-let sum: number = 0;
-for(let student of students) {
-    sum += student.averageGrade;
-
-    if(student.averageGrade > highestAverage) {
-        highestAverage = student.averageGrade;
-        highestAverageStudent = student;
-    }
+enum AccessLevel {
+    Guest = 'Гість',
+    User = 'Користувач',
+    Moderator = 'Модератор',
+    Admin = 'Адміністратор'
 }
-totalAverage = sum / students.length;
 
-console.log(`Середній бал серед усіх студентів: ${totalAverage}`);
-if(highestAverageStudent !== null) {
-    console.log(`Студент з найвищим середнім балом: `, highestAverageStudent);
-} else {
-    console.log('Студент з найвищим середнім балом не знайдений!');
-}
+const currentLevel: AccessLevel = AccessLevel.Moderator;
+
+console.log(`Ваш рівень доступу: ${currentLevel}`);
