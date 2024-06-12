c817ef327f01e46b730c8a1f6f14ef73f88a4a29
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-06-12 18:59:27 +0300

    Objects

--- src/index.js
+++ src/index.js
@@ -1,37 +1,70 @@
-/*
-
-Задача на практику
-
-
-Створіть програму для обчислення вартості замовлення в інтернет-магазині
-
-Декомпозиція
-
-1. Створіть тип даних для представлення товару з назвою та ціною.
-2. Напишіть функцію для обчислення загальної вартості замовлення, яка приймає масив товарів як аргумент та повертає загальну суму
-
-Примітка. Перевірте, що масив товарів не порожній, а ціна кожного товару більше нуля
-
-*/
-function calculateOrderTotal(products) {
-    if (products.length === 0) {
-        console.log('Замовлення порожнє');
-        return 0;
-    }
-    let total = 0;
-    for (const product of products) {
-        if (product.price <= 0) {
-            console.log(`Некоректна ціна для товару "${product.name}"`);
-            continue;
-        }
-        total += product.price;
-    }
-    return total;
-}
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const user = {
+    name: 'John',
+    email: 'john@example.com',
+    password: 'secret'
+};
 const products = [
-    { name: 'Футболка', price: 25 },
-    { name: 'Шорти', price: 30 },
-    { name: 'Носки', price: -5 } // Некоректна ціна
+    { name: 'Футболка', price: 25, quantity: 10 },
+    { name: 'Шорти', price: 30, quantity: 15 },
+    { name: 'Носки', price: 5, quantity: 20 }
 ];
-const orderTotal = calculateOrderTotal(products);
-console.log(orderTotal);
+// Просто якась колекція контактів
+const contacts = [
+    { name: 'John', phone: '123-456-7890' },
+    { name: 'Jane', email: 'jane@example.com' },
+    { name: 'Mike' }
+];
+// Функція для створення нового контакту
+function createContact(name, phone, email) {
+    return {
+        name,
+        phone,
+        email
+    };
+}
+;
+const newContact = createContact('Alice', '987-654-3210', 'alice@example.com');
+// Колекція груп контактів
+const contactsGroup = [
+    { name: 'Friends', contacts: [contacts[0]] },
+    { name: 'Family', contacts: [contacts[1]] }
+];
+// Функція, яка додає контакт до групи
+function addToGroup(contact, groupName) {
+    const group = contactsGroup.find(group => group.name === groupName);
+    if (group) {
+        group.contacts.push(contact);
+    }
+    else {
+        console.log(`Група ${groupName} не знайдена!`);
+    }
+}
+;
+addToGroup(newContact, 'Friends');
+addToGroup(contacts[2], 'Family');
+function fetchUsers() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch('https://randomuser.me/api/?results=100');
+            const data = yield response.json();
+            return data.results;
+        }
+        catch (error) {
+            console.error(error);
+            return [];
+        }
+    });
+}
+fetchUsers()
+    .then((users) => {
+    console.log(users);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,114 @@
-/*
+const user: { name: string, email: string, password: string } = {
+    name: 'John',
+    email: 'john@example.com',
+    password: 'secret'
+};
 
-Задача на практику
+// console.log(user);
 
-
-Створіть програму для обчислення вартості замовлення в інтернет-магазині
-
-Декомпозиція
-
-1. Створіть тип даних для представлення товару з назвою та ціною.
-2. Напишіть функцію для обчислення загальної вартості замовлення, яка приймає масив товарів як аргумент та повертає загальну суму
-
-Примітка. Перевірте, що масив товарів не порожній, а ціна кожного товару більше нуля
-
-*/
 
 type Product = {
     name: string;
     price: number;
-}
-
-function calculateOrderTotal(products: Array<Product>): number {
-    if(products.length === 0) {
-        console.log('Замовлення порожнє');
-        return 0;
-    }
-
-    let total = 0;
-    for(const product of products) {
-        if(product.price <= 0) {
-            console.log(`Некоректна ціна для товару "${product.name}"`);
-            continue;
-        }
-        total += product.price;
-    }
-
-    return total;
-}
+    quantity: number
+};
 
 const products: Array<Product> = [
-    { name: 'Футболка', price: 25 },
-    { name: 'Шорти', price: 30 },
-    { name: 'Носки', price: -5 } // Некоректна ціна
+    { name: 'Футболка', price: 25, quantity: 10 },
+    { name: 'Шорти', price: 30, quantity: 15 },
+    { name: 'Носки', price: 5, quantity: 20 }
 ];
 
-const orderTotal = calculateOrderTotal(products);
-console.log(orderTotal);
+// console.log(products);
+
+
+/*
+
+Задача: Контактна книга з можливістю групування контактів
+
+Створіть програму для управління контактами з можливістю створення груп контактів.
+Кожен контакт має ім'я та може містити телефон та електронну адресу.
+Група контактів - це колекція контактів, які об'єднані за певним критерієм, наприклад, робочі контакти, друзі, родина, тощо
+
+
+*/
+
+type Contact = {
+    name: string;
+    phone?: string;
+    email?: string;
+};
+
+type ContactGroup = {
+    name: string,
+    contacts: Array<Contact>
+};
+
+// Просто якась колекція контактів
+const contacts: Array<Contact> = [
+    { name: 'John', phone: '123-456-7890' },
+    { name: 'Jane', email: 'jane@example.com' },
+    { name: 'Mike' }
+];
+
+// Функція для створення нового контакту
+function createContact(name: string, phone?: string, email?: string): Contact {
+    return {
+        name,
+        phone,
+        email
+    };
+};
+
+const newContact = createContact('Alice', '987-654-3210', 'alice@example.com');
+
+// Колекція груп контактів
+const contactsGroup: Array<ContactGroup> = [
+    { name: 'Friends', contacts: [ contacts[0] ] },
+    { name: 'Family', contacts: [ contacts[1] ] }
+];
+
+// Функція, яка додає контакт до групи
+function addToGroup(contact: Contact, groupName: string) {
+    const group = contactsGroup.find(group => group.name === groupName);
+    if(group) {
+        group.contacts.push(contact);
+    } else {
+        console.log(`Група ${groupName} не знайдена!`);
+    }
+};
+
+addToGroup(newContact, 'Friends');
+addToGroup(contacts[2], 'Family');
+
+// console.log(contactsGroup);
+
+
+
+
+// МЕРЕЖЕВИЙ ЗАПИТ
+
+type User = {
+    name: {
+        first: string;
+        last: string;
+    };
+    email: string;
+};
+
+async function fetchUsers(): Promise<User[]> {
+    try {
+        const response = await fetch('https://randomuser.me/api/?results=100');
+        const data: { results: User[] } = await response.json();
+
+        return data.results;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+fetchUsers()
+.then((users: User[]) => {
+    console.log(users);
+});
