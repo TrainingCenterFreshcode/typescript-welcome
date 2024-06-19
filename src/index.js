f1f338eadc3af669ee2f1ef69c50780bf472e47e
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-06-19 18:01:15 +0300

    Inheritance

--- src/index.js
+++ src/index.js
@@ -1,55 +1,17 @@
-/*
-
-Управління автомобілем:
-Створіть клас Car, що представляє автомобіль з полями:
-- виробник (manufacturer),
-- поточна швидкість (speed),
-- максимальна швидкість (maxSpeed)
-Методи:
-- прискорення
-- сповільнення
-- відображення швидкості
-
-
-*/
-class Car {
-    constructor(manufacturer, maxSpeed) {
-        this._speed = 0;
-        this._manufacturer = manufacturer;
-        this._maxSpeed = maxSpeed;
-    }
-    accelerate(speedDelta) {
-        if (this._speed + speedDelta <= this._maxSpeed) {
-            this._speed += speedDelta;
-        }
-        else {
-            this._speed = this._maxSpeed;
-        }
-    }
-    decelerate(speedDelta) {
-        if (this._speed - speedDelta >= 0) {
-            this._speed -= speedDelta;
-        }
-        else {
-            this._speed = 0;
-        }
-    }
-    get speed() {
-        return this._speed;
-    }
-    get manufacturer() {
-        return this._manufacturer;
-    }
-    get maxSpeed() {
-        return this._maxSpeed;
-    }
-    set manufacturer(value) {
-        this._manufacturer = value;
+// Батьківський клас
+class Animal {
+    constructor(name) {
+        this.name = name;
     }
 }
-const car = new Car('Toyota', 200);
-car.accelerate(60);
-car.accelerate(40);
-car.decelerate(50);
-car.decelerate(50);
-console.log(car.speed);
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name);
+        this.breed = breed;
+    }
+    makeSound() {
+        return `${this.name} barks`;
+    }
+}
+const dog = new Dog('Buddy', 'Golden Retriever');
+console.log(dog.makeSound());
--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,26 @@
-/*
+// Батьківський клас
 
-Управління автомобілем:
-Створіть клас Car, що представляє автомобіль з полями:
-- виробник (manufacturer),
-- поточна швидкість (speed),
-- максимальна швидкість (maxSpeed)
-Методи:
-- прискорення
-- сповільнення
-- відображення швидкості
+abstract class Animal {
+    constructor(
+        public name: string
+    ) {}
 
+    abstract makeSound(): string;
+}
 
-*/
+class Dog extends Animal {
+    breed: string; // Власне поле класу Dog
 
-
-class Car {
-    private _manufacturer: string;
-    private _speed: number;
-    private readonly _maxSpeed: number;
-
-    constructor(manufacturer: string, maxSpeed: number) {
-        this._speed = 0;
-        this._manufacturer = manufacturer;
-        this._maxSpeed = maxSpeed;
+    constructor(name: string, breed: string) {
+        super(name);
+        this.breed = breed;
     }
 
-    public accelerate(speedDelta: number): void {
-        if(this._speed + speedDelta <= this._maxSpeed) {
-            this._speed += speedDelta;
-        } else {
-            this._speed = this._maxSpeed;
-        }
-    }
-
-    public decelerate(speedDelta: number): void {
-        if(this._speed - speedDelta >= 0) {
-            this._speed -= speedDelta;
-        } else {
-            this._speed = 0;
-        }
-    }
-
-    public get speed(): number {
-        return this._speed;
-    }
-
-    public get manufacturer(): string {
-        return this._manufacturer;
-    }
-
-    public get maxSpeed(): number {
-        return this._maxSpeed;
-    }
-
-    public set manufacturer(value: string) {
-        this._manufacturer = value;
+    makeSound(): string {
+        return `${this.name} barks`;
     }
 }
 
-const car = new Car('Toyota', 200);
+const dog = new Dog('Buddy', 'Golden Retriever');
 
-car.accelerate(60);
-car.accelerate(40);
-car.decelerate(50);
-car.decelerate(50);
-
-console.log(car.speed);
+console.log(dog.makeSound());
