5b9b33fbe92c957cf450ae89d5b7dce7240bb026
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-06-26 17:24:41 +0300

    Another example on generics

--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
-function printValue(value) {
-    console.log(`Value: ${value}`);
-}
-const printValueArrow = (value) => {
-    console.log(`Value: ${value}`);
-};
-// printValueArrow(10);
-// printValueArrow('Hello');
-function processValue(value) {
-    if (typeof value === 'number') {
-        console.log(`Value is a number: ${value.toFixed(2)}`);
-    }
-    else if (typeof value === 'string') {
-        console.log(`Value is a string: ${value.toUpperCase()}`);
+/*
+
+Створіть клас DataMerger, який має метод merge, що приймає масиви будь-якого типу та об'єднує їх у один масив
+
+
+*/
+class DataMerger {
+    merge(arr1, arr2) {
+        return [...arr1, ...arr2];
     }
 }
-// processValue(10);
-// processValue('Hello');
-class Box {
-    constructor(value) {
-        this.value = value;
-    }
-    getValue() {
-        return this.value;
-    }
-}
-const box1 = new Box(10);
-const box2 = new Box('Hello');
-console.log(typeof box1.getValue());
-console.log(typeof box2.getValue());
+const dataMerger = new DataMerger();
+const result = dataMerger.merge([1, 2, 3], ['a', 'b', 'c']);
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,18 @@
-function printValue<T>(value: T): void {
-    console.log(`Value: ${value}`);
+
+/*
+
+Створіть клас DataMerger, який має метод merge, що приймає масиви будь-якого типу та об'єднує їх у один масив
+
+
+*/
+
+class DataMerger<T0, T1> {
+    merge(arr1: T0[], arr2: T1[]): (T0 | T1)[] {
+        return [...arr1, ...arr2];
+    } 
 }
 
-const printValueArrow = <T>(value: T): void => {
-    console.log(`Value: ${value}`);
-}
+const dataMerger = new DataMerger();
+const result = dataMerger.merge([1, 2, 3], ['a', 'b', 'c']);
 
-// printValueArrow(10);
-// printValueArrow('Hello');
-
-
-
-function processValue<T extends number | string>(value: T): void {
-    if(typeof value === 'number') {
-        console.log(`Value is a number: ${value.toFixed(2)}`);
-    } else if (typeof value === 'string') {
-        console.log(`Value is a string: ${value.toUpperCase()}`);
-    }
-}
-
-// processValue(10);
-// processValue('Hello');
-
-
-
-class Box<T> {
-    value: T;
-
-    constructor(value: T) {
-        this.value = value;
-    }
-
-    getValue(): T {
-        return this.value;
-    }
-}
-
-const box1 = new Box<number>(10);
-const box2 = new Box<string>('Hello');
-
-console.log(typeof box1.getValue());
-console.log(typeof box2.getValue());
+console.log(result);
